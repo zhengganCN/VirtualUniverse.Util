079f0b27f75ea30ccc32a4b6a2bd0f7fccb0a7f8
zhengganCN <932813593@qq.com>
2020-12-02 16:49:33 +0800

    权限验证优化

--- AmazedSecurity/Permission/PermissionAttribute.cs
+++ AmazedSecurity/Permission/PermissionAttribute.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -58,16 +59,25 @@ namespace AmazedSecurity.Permission
         /// <param name="context">执行前上下文</param>
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            var auth = context.HttpContext.Request.Headers["Authorization"];
-            var jwt = new JwtSecurityToken(auth.ToString().Split(' ')[1]);
-            var userId = long.Parse(jwt.Subject);
-
-            IsValid = ValidUserPermission(userId.ToString(), Namespace + "." + PermissionName);
-            if (!IsValid)
+            var result = new ModelResult<string>();
+            try
             {
-                var result = new ModelResult<string>();
-                result.FailedResult<string>(null, "权限不足");
+                var auth = context.HttpContext.Request.Headers["Authorization"];
+                var jwt = new JwtSecurityToken(auth.ToString().Split(' ')[1]);
+                var userId = long.Parse(jwt.Subject);
+
+                IsValid = ValidUserPermission(userId.ToString(), Namespace + "." + PermissionName);
+                if (!IsValid)
+                {
+                    result.FailedResult<string>(null, "权限不足");
+                    context.Result = new JsonResult(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                result.FailedResult<string>(null, "权限验证失败，服务器异常");
                 context.Result = new JsonResult(result);
+                new LoggerFactory().CreateLogger<PermissionAttribute>().LogError(ex.ToString());
             }
         }
     }
