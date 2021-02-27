f16d342ba5850bd64a4a726517ba44d5187a3d7b
zhengganCN <932813593@qq.com>
2021-02-27 15:07:23 +0800

    新增系统类库扩展

---
+++ VirtualUniverse.Extension/VirtualUniverse.Extension.System/DirectoryExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+/***********************************************************************************
+****作者：zhenggan；创建时间：2021/2/24 23:37:37；更新时间：
+************************************************************************************/
+namespace VirtualUniverse.Extension.System
+{
+    /// <summary>
+    /// 类说明：目录扩展
+    /// </summary>
+    public static class DirectoryExtension
+    {
+        /// <summary>
+        /// 获取文件夹路径
+        /// </summary>
+        /// <param name="path">根目录</param>
+        /// <param name="deep">递归深度；当为null时或小于等于0时，不递归目录</param>
+        /// <returns></returns>
+        public static IList<string> GetDirectories(string path, int? deep = null)
+        {
+            var directoryPaths = Directory.GetDirectories(path);
+            return GetDirectories(directoryPaths, deep);
+        }
+        /// <summary>
+        /// 获取文件夹路径
+        /// </summary>
+        /// <param name="directoryPaths">目录路径</param>
+        /// <param name="deep">递归深度；当为null时或小于等于0时，不递归目录</param>
+        /// <returns></returns>
+        private static List<string> GetDirectories(IList<string> directoryPaths, int? deep = null)
+        {
+            var allOfDirectoryPaths = new List<string>();
+            allOfDirectoryPaths.AddRange(directoryPaths);
+            if (deep.HasValue && --deep < 0)
+            {
+                return allOfDirectoryPaths;
+            }
+            foreach (var directoryPath in directoryPaths)
+            {
+                var childDirectoryPaths = GetDirectories(directoryPath, deep);
+                allOfDirectoryPaths.AddRange(childDirectoryPaths);
+            }
+            return allOfDirectoryPaths;
+        }
+    }
+}
---
+++ VirtualUniverse.Extension/VirtualUniverse.Extension.System/EnumExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+
+/***********************************************************************************
+****作者：zhenggan；创建时间：2021/2/27 14:23:08；更新时间：
+************************************************************************************/
+namespace VirtualUniverse.Extension.System
+{
+    /// <summary>
+    /// 枚举扩展类
+    /// </summary>
+    public static class EnumExtension
+    {
+        /// <summary>
+        /// 获取枚举描述
+        /// </summary>
+        /// <param name="value">枚举值</param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum value)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+            var field = value.GetType().GetField(value.ToString());
+            var description = ((DescriptionAttribute)field.GetCustomAttribute(typeof(DescriptionAttribute)))?.Description;
+            return !string.IsNullOrEmpty(description) ? description : field.Name;
+        }
+    }
+}
