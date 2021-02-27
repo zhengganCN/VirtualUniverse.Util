2191bb3a492451fd3fb34f167ec31676a3e040b8
zhengganCN <932813593@qq.com>
2021-02-27 15:06:13 +0800

    添加注释

--- VirtualUniverse.Repository/VirtualUniverse.Repository.Mongo/DbContext.cs
+++ VirtualUniverse.Repository/VirtualUniverse.Repository.Mongo/DbContext.cs
@@ -1,6 +1,5 @@
 ﻿using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 /***********************************************************************************
@@ -9,18 +8,33 @@ using System.Text;
 namespace VirtualUniverse.Repository.Mongo
 {
     /// <summary>
-    /// 类 描 述：
+    /// 类 描 述：上下文
     /// </summary>
-    public abstract class DbContext
+    public abstract class DbContext : IDisposable
     {
+        /// <summary>
+        /// 客户端
+        /// </summary>
         public IMongoClient MongoClient { get; private set; }
+        /// <summary>
+        /// 数据库
+        /// </summary>
         public IMongoDatabase MongoDatabase { get; private set; }
         private readonly DbContextOptionsBuilder contextOptionsBuilder = new DbContextOptionsBuilder();
+        private bool disposedValue;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
         protected DbContext()
         {
             Init();
         }
 
+        /// <summary>
+        /// 配置构造
+        /// </summary>
+        /// <param name="builder"></param>
         protected abstract void OnConfiguring(DbContextOptionsBuilder builder);
 
         private void Init()
@@ -40,11 +54,24 @@ namespace VirtualUniverse.Repository.Mongo
             return default;
         }
 
+        /// <summary>
+        /// 获取表对象
+        /// </summary>
+        /// <typeparam name="TEntity">实体</typeparam>
+        /// <param name="settings">设置</param>
+        /// <returns></returns>
         public IMongoCollection<TEntity> GetMongoCollection<TEntity>(MongoCollectionSettings settings = null)
         {
             return MongoDatabase.GetCollection<TEntity>(typeof(TEntity).Name, settings);
         }
-
+        /// <summary>
+        /// 获取表对象
+        /// </summary>
+        /// <typeparam name="TEntity">实体</typeparam>
+        /// <param name="databaseName">数据库名称</param>
+        /// <param name="mongoDatabaseSettings">数据库设置</param>
+        /// <param name="mongoCollectionSettings">表设置</param>
+        /// <returns></returns>
         public IMongoCollection<TEntity> GetMongoCollection<TEntity>(string databaseName,
             MongoDatabaseSettings mongoDatabaseSettings = null, MongoCollectionSettings mongoCollectionSettings = null)
         {
@@ -76,6 +103,31 @@ namespace VirtualUniverse.Repository.Mongo
                     }
                 }
             }
+        }
+        /// <summary>
+        /// 释放
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // 释放托管状态(托管对象)
+                }
+
+                disposedValue = true;
+            }
+        }
+        /// <summary>
+        /// 释放
+        /// </summary>
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
