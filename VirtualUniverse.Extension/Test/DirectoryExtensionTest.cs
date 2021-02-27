fb8f1164e8eaf756ff2cde3ccaa667872e0ef1f1
zhengganCN <932813593@qq.com>
2021-02-27 15:08:20 +0800

    移除无用类库

---
+++ VirtualUniverse.Extension/Test/DirectoryExtensionTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VirtualUniverse.Extension.System;
+
+/***********************************************************************************
+****作者：zhenggan；创建时间：2021/2/24 23:46:33；更新时间：
+************************************************************************************/
+namespace Test
+{
+    /// <summary>
+    /// 类说明：
+    /// </summary>
+    class DirectoryExtensionTest
+    {
+        [Test]
+        public void ReadDirectoriesTest()
+        {
+            var s = DirectoryExtension.GetDirectories(@"G:\Softwares\VirtualUniverse.Util", 1);
+        }
+    }
+}
